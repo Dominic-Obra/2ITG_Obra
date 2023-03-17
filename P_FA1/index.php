287ff604b4769d9ef393a32669328932b8190a6d
Dominic-Obra <dominic.obra.cics@ust.edu.ph>
2023-03-18 03:06:09 +0800

    Activity complete

--- P_FA1/index.php
+++ P_FA1/index.php
@@ -1,40 +1,117 @@
 <?php
 
-class nameOfClass{
-    private $memberA;
-    private string $memberB;
-    private int $memberC;
+class Phone{
+    private bool $hasBattery;
+    private string $nameOfOwner;
+    private int $phoneNumber;
 
     public function displayInfo(){
-        echo $memberA . " " . $memberB . " " . $memberC;
+        echo "This phone has battery: ". (true === (bool)$this->hasBattery ? 'True' : 'False') . "<br>Owner: " . 
+        $this->nameOfOwner . "<br>Phone#:" . $this->phoneNumber;
     }
 
-    public function setMemberA($memberA){
-        $this->memberA = $memberA;
+    public function setHasBattery($hasBattery){
+        $this->hasBattery = $hasBattery;
      }
-     public function getMemberA(){
-        return $this->memberA;
-     }
-
-     public function setMemberB($memberB){
-        $this->memberB = $memberB;
-     }
-     public function getMemberB(){
-        return $this->memberB;
+     public function getHasBattery(){
+        return $this->hasBattery;
      }
 
-     public function setMemberC($memberC){
-        $this->memberC = $memberC;
+     public function setNameOfOwner($nameOfOwner){
+        $this->nameOfOwner = $nameOfOwner;
      }
-     public function getMemberC(){
-        return $this->memberC;
+     public function getNameOfOwner(){
+        return $this->nameOfOwner;
+     }
+
+     public function setPhoneNumber($phoneNumber){
+        $this->phoneNumber = $phoneNumber;
+     }
+     public function getPhoneNumber(){
+        return $this->phoneNumber;
      }
 }
 
-$sampleObject = new nameOfClass();
-$sampleObject->setMemberA("Hello");
-$sampleVar = $sampleObject->getMemberA();
+class SailfishOS extends Phone{
+    private bool $isOpenSource;
 
-echo $sampleVar;
+    function __construct($parameter){
+        $this->setIsOpenSource($parameter);
+    }
+    public function setIsOpenSource($isOpenSource){
+        $this->isOpenSource = $isOpenSource;
+     }
+    public function getIsOpenSource(){
+        return $this->isOpenSource;
+     }
+    public function printInfo(){
+        echo "<br>OS is open source: ". (true === (bool)$this->getIsOpenSource()?'True' : 'False')."";
+     }
+    public function displayInfo(){
+        echo "<br>SAILFISH OS PHONE"."<br>This phone has battery: ". (true === (bool)$this->getHasBattery() ? 'True' : 'False') . "<br>Owner: " . 
+        $this->getNameOfOwner() . "<br>Phone#:" . $this->getPhoneNumber();
+    }
+}
+class Android extends Phone {
+    public function displayInfo(){
+        echo "<br><br>ANDROID PHONE"."<br>This phone has battery: ". (true === (bool)$this->getHasBattery() ? 'True' : 'False') 
+        . "<br>Owner: " . 
+        $this->getNameOfOwner() . "<br>Phone#:" . $this->getPhoneNumber();
+    }
+    public function setIsOpenSource($isOpenSource){
+        $this->isOpenSource = $isOpenSource;
+     }
+    public function getIsOpenSource(){
+        return $this->isOpenSource;
+     }
+}
+class IOS extends Phone {
+    public function displayInfo(){
+        echo "<br><br>IOS PHONE"."<br>This phone has battery: ". (true === (bool)$this->getHasBattery() ? 'True' : 'False') 
+        . "<br>Owner: " . 
+        $this->getNameOfOwner() . "<br>Phone#:" . $this->getPhoneNumber();
+    }
+    public function setIsOpenSource($isOpenSource){
+        $this->isOpenSource = $isOpenSource;
+     }
+    public function getIsOpenSource(){
+        return $this->isOpenSource;
+     }
+}
+class Samsung extends Android{
+    public function displayInfo(){
+        echo "<br><br>SAMSUNG: ANDROID PHONE"."<br>This phone has battery: ". (true === (bool)$this->getHasBattery() ? 'True' : 'False') . "<br>Owner: " . 
+        $this->getNameOfOwner() . "<br>Phone#:" . $this->getPhoneNumber().
+        "<br>OS is open source: ". (true === (bool)$this->getIsOpenSource()?'True' : 'False')."";;
+    }
+}
+class Apple extends IOS{
+    public function displayInfo(){
+        echo "<br><br>APPLE: IOS PHONE"."<br>This phone has battery: ". (true === (bool)$this->getHasBattery() ? 'True' : 'False') 
+        . "<br>Owner: " . 
+        $this->getNameOfOwner() . "<br>Phone#:" . $this->getPhoneNumber().
+         "<br>OS is open source: ". (true === (bool)$this->getIsOpenSource()?'True' : 'False')."";;
+    }
+}
 
+$phone1 = new SailfishOS(true);
+$phone1->setHasBattery(true);
+$phone1->setNameOfOwner("Jean");
+$phone1->setPhoneNumber(84311512711);
+$phone1->displayInfo();
+$phone1->printInfo();
+
+$phone2 = new Samsung();
+$phone2->setHasBattery(true);
+$phone2->setNameOfOwner("John");
+$phone2->setPhoneNumber(67348123513);
+$phone2->setIsOpenSource(true);
+$phone2->displayInfo();
+
+$phone3 = new Apple();
+$phone3->setHasBattery(true);
+$phone3->setNameOfOwner("Jack");
+$phone3->setPhoneNumber(81247345681);
+$phone3->setIsOpenSource(false);
+$phone3->displayInfo();
 ?>
